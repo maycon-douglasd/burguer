0b4e00851a2218f76c37ff0fd743811f28af3f69
maycon-douglasd <maycon_douglasds@hotmail.com>
2024-06-13 19:56:26 -0300

    Burguer atualizado

--- src/js/index-products.js
+++ src/js/index-products.js
@@ -1,15 +1,15 @@
 const menuOptions = [
 
-    { name: 'X-Salada', price: 30, vegan: false, src: './img/xsalada.jpeg' },
+    { name: 'X-Salada', price: 28, vegan: false, src: './src/img/xsalada.jpeg' },
     
-    { name: 'X-Bacon', price: 34, vegan: false, src: './img/xbacon.png' },
+    { name: 'X-Bacon', price: 30, vegan: false, src: './src/img/xbacon.png' },
     
-    { name: 'X-Bacon Egg', price: 39, vegan: false, src: './img/bacon-egg.png' },
+    { name: 'X-Bacon Egg', price: 38.99, vegan: false, src: '/src/img/bacon-egg.png' },
     
-    { name: 'Monstruoso', price: 50, vegan: false, src: './img/monstruoso.png' },
+    { name: 'Monstruoso', price: 37.50, vegan: false, src: './src/img/monstruoso.png' },
     
-    { name: 'Big Vegano', price: 55, vegan: true, src: './img/xvegan.png' },
+    { name: 'Big Vegano', price: 55, vegan: true, src: './src/img/xvegan.png' },
     
-    { name: 'X-Vegan', price: 45, vegan: true, src: './img/monstruoso-vegan.png' },
+    { name: 'X-Vegan', price: 24.90, vegan: true, src: './src/img/monstruoso-vegan.png' },
     
     ]
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,63 @@
 const list = document.querySelector("ul")
-const product = { name: 'X-Salada', price: 30, vegan: false, src: './img/xsalada.jpeg' }
+const buttonShowAll = document.querySelector(".show-all")
+const buttonMapAll = document.querySelector(".map-all")
+const sumAll = document.querySelector(".sum-all")
+const filterAll = document.querySelector(".filter-all")
 
-list.innerHTML = `
-    <li>
-        <img src=${product.src}>
-        <p>${product.name}</p>
-        <p class="item-price">R$ ${product.price}</p>
-    </li>
+function formatCurrency(value) {
+    const newValue = value.toLocaleString("pt-br", { style: "currency", 
+    currency: "BRL" })
 
-`
+    return newValue
+}
+
+function showAll(productsArray) {
+    let myLi = ''
+
+    productsArray.forEach((product) => {
+        myLi = myLi + `
+     
+             <li>
+                 <img src=${product.src}>
+                 <p>${product.name}</p>
+                 <p class="item-price">R$ ${formatCurrency(product.price)}</p>
+             </li>
+         
+         `
+    })
+
+    list.innerHTML = myLi
+}
+
+function mapAllItems() {
+    const newPrices = menuOptions.map((product) => ({
+        ...product,
+        price: product.price * 0.9,
+    }))
+
+    showAll(newPrices)
+}
+
+function sumAllItems() {
+    const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+
+    list.innerHTML = `
+        <li>
+           <p>O Valor total dos itens é R$ ${formatCurrency(totalValue)}</p>
+        </li>
+
+    `
+
+    console.log(totalValue)
+}
+
+function filterAllItems() {
+    const filterJustVegan = menuOptions.filter((product) => product.vegan === true)
+
+    showAll(filterJustVegan)
+}
+
+buttonShowAll.addEventListener("click", () => showAll(menuOptions))
+buttonMapAll.addEventListener("click", mapAllItems)
+sumAll.addEventListener("click", sumAllItems)
+filterAll.addEventListener("click", filterAllItems)
